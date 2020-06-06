10f53dd2509028674f8f527a5e381e9b54138e74
Kemp <32265362+kempspo@users.noreply.github.com>
2020-06-07 01:32:18 +0800

    Adding notes and blog view to urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from blog.views import PostDetail, PostList, TagList
+from blog.views import PostDetail, List, TagList, NoteDetail, PostList
 from django.urls import path
 
 urlpatterns = [
-    path('', PostList.as_view(), name='home'),
+    path('', List.as_view(), name='home'),
+    path('blog/', PostList.as_view(), name='post_list'),
     path('blog/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('tag/<slug:slug>/', TagList.as_view(), name='tagged'),
+    path('note/<slug:slug>/', NoteDetail.as_view(), name='note_detail'),
 ]
