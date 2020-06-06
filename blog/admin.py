613f766aa13f275ace5785f20728717d507ea16f
Kemp <32265362+kempspo@users.noreply.github.com>
2020-06-07 01:31:59 +0800

    Add notes to admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Note
 from markdownx.admin import MarkdownxModelAdmin
 
 @admin.register(Post)
@@ -8,5 +8,11 @@ class PostAdmin(MarkdownxModelAdmin):
     list_filter = ('status','created_on','tag') 
     search_fields = ['title', 'content', 'tag']
     prepopulated_fields = {'slug': ('title',)}
-  
-# admin.site.register(Post, PostAdmin)
+
+
+@admin.register(Note)
+class NoteAdmin(MarkdownxModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on','updated_on')
+    list_filter = ('status','created_on') 
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
