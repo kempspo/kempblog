213da47d9080339c9c660ab4ac37150badd87f64
Kemp <32265362+kempspo@users.noreply.github.com>
2020-06-07 01:32:54 +0800

    Add new models to views

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,24 @@
 from django.views.generic import ListView, DetailView
-from blog.models import Post
+from blog.models import Post, Note
 from taggit.models import Tag
+from itertools import chain
 
-class PostList(ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+class List(ListView):
+    model = Post
+    def get_context_data(self, *args, **kwargs):
+        context = super(List, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(status=1).order_by('-created_on')
+        context['notes'] = Note.objects.filter(status=1).order_by('-created_on')
+        return context
     template_name = 'index.html'
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostList(DetailView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'post_list.html'
 
 class TagList(ListView):
     template_name = 'index.html'
@@ -19,3 +29,7 @@ class TagList(ListView):
         queryset = Post.objects.filter(tag__name__in=tags)
 
         return queryset
+
+class NoteDetail(DetailView):
+    model = Note
+    template_name = 'note_detail.html'
