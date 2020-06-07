acc200262cd2955a6c1737a401a0bbfcb5f830a4
Kemp <32265362+kempspo@users.noreply.github.com>
2020-06-07 16:19:50 +0800

    Add paths to contact and about

--- kempblog/urls.py
+++ kempblog/urls.py
@@ -17,10 +17,12 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('about/', TemplateView.as_view(template_name="about.html")),
+    path('about/', TemplateView.as_view(template_name="about.html")),
+    path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('', include('blog.urls')),
     path('markdownx/', include('markdownx.urls')),
 ]
