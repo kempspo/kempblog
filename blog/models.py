6aff3e83b8ce3aa881e854a65f0171c72d8fa4aa
Kemp <32265362+kempspo@users.noreply.github.com>
2020-06-24 23:30:35 +0800

    Add blog intro

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    intro = models.TextField(max_length=1200)
     text = MarkdownxField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
